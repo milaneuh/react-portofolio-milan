636d53e76a70969a312198058b4f88be3d00d178
milaneuh <82022681+milaneuh@users.noreply.github.com>
2021-10-30 13:38:30 +0200

    Home and notFound pages styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,19 @@ import Navigation from '../components/Navigation';
 
 const Home = () => {
     return (
-        <div>
+        <div className="home">
             <Navigation/>
+            <div className="homeContent">
+                <div className="content">
+                    <h1>Milan Rougemont</h1>
+                    <h2>Web & Mobile Developer</h2>
+                    <div className="pdf">
+                        <a href="./media/CVMILAN.pdf" target="_blank">
+                            Download Resume
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 const Notfound = () => {
     return (
-        <div>
-            404 c'est pas de bol
+        <div className="notFound">
+            <div className="notFoundContent">
+                <h3>Désolé, cette page n'existe pas</h3>
+                <NavLink exact to="/">
+                    <i className="fas fa-home"></i>
+                    <span>Home</span>
+                </NavLink>
+            </div>
         </div>
     );
 };
